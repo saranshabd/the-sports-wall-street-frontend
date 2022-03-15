b2cb209e98bcbbbfdc1dcc8106a80fd9b3929ab0
Shabd Saran <saranshabd@gmail.com>
2022-03-15 22:06:03 +0530

    chore: remove PUBLIC_URL from sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -195,8 +195,8 @@ function Sidebar(props) {
   var brand = (
     <Box pt={"25px"} mb='12px'>
       <Link
-        href={`${process.env.PUBLIC_URL}/#/`}
-        target='_blank'
+        href={`/`}
+        // target='_blank'
         display='flex'
         lineHeight='100%'
         mb='30px'
